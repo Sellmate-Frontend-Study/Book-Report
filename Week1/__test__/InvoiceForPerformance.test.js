edb652ff81ce28eb1dc3cd62c7a9f29ab48d755e
sonsu95 <tjdtn951007@gmail.com>
2024-01-07 23:35:27 +0900

    feat: 테스트 코드 변경 toBe => toEqual

--- Week1/__test__/InvoiceForPerformance.test.js
+++ Week1/__test__/InvoiceForPerformance.test.js
@@ -3,7 +3,7 @@ const plays = require("../MockData/plays.json");
 const { statement } = require("../Example/InvoiceForPerformance");
 
 describe('statement 함수 테스트', () => {
-	test('일반적인 청구서를 올바르게 처리해야 함', () => {
+	test('statement 함수 결과 테스트', () => {
 		const expected = `청구 내역 (고객명: BigCo\n` +
 			`Hamlet: $650.00 (55석)\n` +
 			`As You Like It: $580.00 (35석)\n` +
@@ -12,6 +12,6 @@ describe('statement 함수 테스트', () => {
 			`적립 포인트: 47점\n`;
 		
 		const result = statement(invoices[0], plays);
-		expect(result).toBe(expected);
+		expect(result).toEqual(expected);
 	});
 });
