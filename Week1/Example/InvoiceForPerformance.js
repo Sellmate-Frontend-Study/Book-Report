be4f990490bf03102f8ab0dd50380dd0fdc6fa09
sonsu95 <tjdtn951007@gmail.com>
2024-01-08 02:10:57 +0900

    refactor: totalAmount 변수 함수 처리 (임시 함수명 사용)

--- Week1/Example/InvoiceForPerformance.js
+++ Week1/Example/InvoiceForPerformance.js
@@ -11,6 +11,14 @@
  */
 export function statement(invoice, plays) {
 	
+	function appleSauce() {
+		let totalAmount = 0;
+		for (let perf of invoice.performances) {
+			totalAmount += amountFor(perf);
+		}
+		return totalAmount;
+	}
+	
 	/**
 	 * @returns {number}
 	 */
@@ -81,15 +89,11 @@ export function statement(invoice, plays) {
 		return result;
 	}
 	
-	let totalAmount = 0;
 	let result = `청구 내역 (고객명: ${invoice.customer}\n`;
-	
 	for (let perf of invoice.performances) {
-		
-		// 청구 내역을 출력한다.
 		result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-		totalAmount += amountFor(perf);
 	}
+	let totalAmount = appleSauce();
 	
 	result += `총액: ${usd(totalAmount)}\n`;
 	result += `적립 포인트: ${totalVolumeCredits()}점\n`;
