6ce5bb17b359b5a18a4d991c761760bb92f66259
sonsu95 <tjdtn951007@gmail.com>
2024-01-08 00:01:10 +0900

    refactor: amountFor 파라미터명 변경
    perf => aPerformance

--- Week1/Example/InvoiceForPerformance.js
+++ Week1/Example/InvoiceForPerformance.js
@@ -7,27 +7,28 @@
 export function statement(invoice, plays) {
 	
 	/**
-	 * @param { Object } perf - 공연 내역
-	 * @param { number } perf.audience - 공연 관객 수
-	 * @param { string } perf.playID - 공연 정보
+	 * @param { Object } aPerformance - 공연 내역
+	 * @param { number } aPerformance.audience - 공연 관객 수
+	 * @param { string } aPerformance.playID - 공연 정보
 	 * @param { Object } play - 공연 정보를 담고 있는 객체 (각 playID에 대응하는 정보 포함)
+	 * @returns { number }
 	 */
-	function amountFor(perf, play) {
+	function amountFor(aPerformance, play) {
 		let result = 0;
 		
 		switch (play.type) {
 			case 'tragedy':
 				result = 40000;
-				if (perf.audience > 30) {
-					result += 1000 * (perf.audience - 30);
+				if (aPerformance.audience > 30) {
+					result += 1000 * (aPerformance.audience - 30);
 				}
 				break;
 			case 'comedy':
 				result = 30000;
-				if (perf.audience > 20) {
-					result += 10000 + 500 * (perf.audience - 20);
+				if (aPerformance.audience > 20) {
+					result += 10000 + 500 * (aPerformance.audience - 20);
 				}
-				result += 300 * perf.audience;
+				result += 300 * aPerformance.audience;
 				break;
 			default:
 				throw new Error(`알 수 없는 장르: ${play.type}`);
